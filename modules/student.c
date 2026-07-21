882a2d8db31b8bbdee5e5bb86b84c0f6e5f67c15
Aïcha Bakayoko <aichabakayoko@example.com>
2026-07-21 21:42:32 +0600

    Implemented student ranking sort algorithm

--- modules/student.c
+++ modules/student.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "student.h"
+#include "gpa.h"
 
 Student createStudent(char id[], char name[])
 {
@@ -23,5 +24,24 @@ void viewStudent(Student student)
     for (int i = 0; i < student.n_results; i++)
     {
         viewCourseResult(student.results[i]);
+    }
+}
+
+void sortStudentsByCGPA(Student students[], int n_students)
+{
+    for (int i = 0; i < n_students - 1; i++)
+    {
+        for (int j = 0; j < n_students - i - 1; j++)
+        {
+            double cgpa1 = calculateCGPA(students[j].results, students[j].n_results);
+            double cgpa2 = calculateCGPA(students[j + 1].results, students[j + 1].n_results);
+
+            if (cgpa1 < cgpa2)
+            {
+                Student temp = students[j];
+                students[j] = students[j + 1];
+                students[j + 1] = temp;
+            }
+        }
     }
 }
